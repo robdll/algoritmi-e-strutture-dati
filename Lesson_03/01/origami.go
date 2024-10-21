2ef3030a9278bec0accd0114f17663a6f2d81bac
Roberto Di Lillo <roberto.dilillo@garycom.com>
2024-10-21 14:50:38 +0200

    update origami

--- Lesson_03/01/origami.go
+++ Lesson_03/01/origami.go
@@ -23,16 +23,15 @@ func main() {
 	var instructions []Instruction
 	var coordinates []Coord
 	coordinateMap := make(map[string]bool)
+	maxColumn, maxRow := 0, 0
 
-	file, err := os.Open("./Lesson_03/01/input_small.txt")
+	// lettura file
+	file, err := os.Open("./Lesson_03/01/input.txt")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return
 	}
 	scanner := bufio.NewScanner(file)
-
-	maxColumn, maxRow := 0, 0
-
 	for scanner.Scan()  {
 		line := scanner.Text()
 
@@ -62,9 +61,6 @@ func main() {
 		}
 	}
 
-	// fmt.Println("instructions", instructions)
-	// fmt.Println("coordinates", coordinates)
-
 	// create paper and draw coordinates
 	paper := make([][]string, maxRow+1)
 	for i:=0; i<=maxRow; i++ {
@@ -78,65 +74,49 @@ func main() {
 			}
 		}
 	}
+	
+	// esegui piegature
+	for i:=0; i<len(instructions); i++ {
+		isVerticalLine := instructions[i].direction == "x"
+		foldingRow := len(paper)
+		foldingColumn := len(paper[0])
+		if isVerticalLine {
+			xLine := instructions[i].index
+			foldingColumn = xLine
+		} else {
+			yLine := instructions[i].index
+			foldingRow = yLine
+		}
+		for j:=0; j<foldingRow; j++ {
+			for k:=0; k<foldingColumn; k++ {
+				var overlapValue string 
+				if isVerticalLine {
+					overlapValue = paper[j][maxColumn-k]
+				} else {
+					overlapValue = paper[maxRow-j][k]
+				}
+				if overlapValue == "#" {
+					paper[j][k] = "#"
+				}
+			}
+			// scarto tutte le colonne non necessarie qui
+		}
+		// scarto tutte le righe non necessarie qui
+		if(!isVerticalLine) {
+			paper = paper[0:foldingRow]
+			maxRow = len(paper) -1
+		} else {
+			for j:=0; j<=maxRow; j++ {
+				paper[j] = paper[j][0:foldingColumn]
+			}
+			maxColumn = len(paper[0]) -1
+		}
+	}
 
+
+	// print the grid
 	for i:=0; i<=maxRow; i++ {
 		fmt.Println(strings.Join(paper[i], ""))
 	}
 
-	// // per ogni istruzione di piegatura
-	// for i:=0; i<len(foldingSequence); i++ {
-	// 	// per ogni riga della carta o riga/2 se pieghi lungo y
-	// 	// check first char of foldingSequence to see if it's x or y
-
-	// 	isVerticalLine := strings.Split(foldingSequence[i], "=")[0] == "x"
-	// 	foldingRow := len(paper)
-	// 	foldingColumn := len(paper[0])
-	// 	if isVerticalLine {
-	// 		xLine, _ := strconv.Atoi(strings.Split(foldingSequence[i], "=")[1])
-	// 		foldingColumn = xLine
-	// 	} else {
-	// 		yLine, _ := strconv.Atoi(strings.Split(foldingSequence[i], "=")[1])
-	// 		foldingRow = yLine
-	// 	}
-	// 	for j:=0; j<foldingRow; j++ {
-	// 		for k:=0; k<foldingColumn; k++ {
-	// 			var overlapValue string 
-	// 			if isVerticalLine {
-	// 				// fmt.Println("foldingColumn", foldingColumn)
-	// 				overlapValue = paper[j][maxColumn-k]
-	// 			} else {
-	// 				overlapValue = paper[maxRow-j][k]
-	// 			}
-	// 			if overlapValue == "#" {
-	// 				paper[j][k] = "#"
-	// 			}
-	// 		}
-	// 		// scarto tutte le colonne non necessarie qui
-			
-	// 	}
-	// 	// scarto tutte le righe non necessarie qui
-	// 	if(!isVerticalLine) {
-	// 		paper = paper[0:foldingRow]
-	// 		maxRow = len(paper) -1
-	// 	} else {
-	// 		for j:=0; j<=maxRow; j++ {
-	// 			paper[j] = paper[j][0:foldingColumn]
-	// 		}
-	// 		maxColumn = len(paper[0]) -1
-	// 	}
-	// }
-
-
-	// // print the map
-	// for i:=0; i<=maxRow; i++ {
-	// 	fmt.Println(strings.Join(paper[i], ""))
-	// 	// riga lunghezza meno i
-	// 	// fmt.Println(strings.Join(paper[maxRow-i], ""))
-	// }
-
-	
 }
-
-// avrei fatto un ciclo for iniziale per vedere se vuole che tagli alla y o x fino a che boh prema un tasto che vuole per uscire dalla piegatura, poi if che appunto
-// mi dice se ha scelto un x o y, e dentro ognuno facevi un for per appunto fare il fold facendo poi vedere allutente la piegatura corrente e poi di nuovo la domanda
-// almeno cosi fai il ciclo for per y o per x
