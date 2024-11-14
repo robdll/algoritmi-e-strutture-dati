51499090596f802b30d73de658682d4d14018183
Roberto Di Lillo <roberto.dilillo@garycom.com>
2024-11-14 09:22:00 +0100

    postfix calc with stack

--- Lesson_12/01/main.go
+++ Lesson_12/01/main.go
@@ -3,20 +3,52 @@ package main
 import (
 	"fmt"
 	"stack_project/stack"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 func main() {
 	s := stack.NewStack()
+	sequence := "2 5 3 - *"
+	CalcExpression(s, strings.Split(sequence, " "))
+}
 
-	s.Push(10)
-	s.Push(20)
-	s.Push(30)
 
-	top, _ := s.Peek()
-	fmt.Println("Top element:", top) // Should print "Top element: 30"
+func CalcExpression (s *stack.Stack, seq []string) {
+	for _, token := range  seq {
+		fmt.Println("Token:", token)
+		digit := rune(token[0])
+		if unicode.IsDigit(digit) {
+			num, _ := strconv.Atoi(token)
+			s.Push(num)
+			fmt.Println("Token added:", num)
+		} else {
+			value2, _ := s.Pop()
+			value1, _ := s.Pop()
+			switch token {
+			case "+":
+				fmt.Println("Adding:", value1, value2)
+				s.Push(value1 + value2)
+			case "-":
+				fmt.Println("Subtracting:", value1, value2)
+				s.Push(value1 - value2)
+			case "*":
+				fmt.Println("Multiplying:", value1, value2)
+				s.Push(value1 * value2)
+			case "/":
+				fmt.Println("Dividing:", value1, value2)
+				s.Push(value1 / value2)
+			}
+			newValue, _ := s.Peek()
+			fmt.Println("Added value to stack:", newValue)
+		}
+	}
 
-	for !s.IsEmpty() {
-		val, _ := s.Pop()
-		fmt.Println("Popped:", val)
+	if s.Len() == 1 {
+		result, _ := s.Pop()
+		fmt.Println("Postfixed Expression result:", result)
+	} else {
+		fmt.Println("Error in expression")
 	}
 }
