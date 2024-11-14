429e5505cc6fa5c743de5ce0b549f598dccb3ebc
Roberto Di Lillo <roberto.dilillo@garycom.com>
2024-11-14 10:21:46 +0100

    check html

--- Lesson_12/01/main.go
+++ Lesson_12/01/main.go
@@ -9,13 +9,47 @@ import (
 )
 
 func main() {
-	sequence := "2 5 3 - *"
-	sequenceInfix := "2 * ( 5 - 3 )"
-	convertedToPostfix := converti(sequenceInfix)	
-	fmt.Println("sequenceInfix in Postfixed:", convertedToPostfix)
-	valuta(sequence)
+	// sequence := "2 5 3 - *"
+	// sequenceInfix := "2 * ( 5 - 3 )"
+	htmlString := "<a> <b> </b> </c>"
+	checkHtml(htmlString)
+	// convertedToPostfix := converti(sequenceInfix)	
+	// fmt.Println("sequenceInfix in Postfixed:", convertedToPostfix)
+	// valuta(sequence)
 }
 
+func checkHtml(html string) {
+	fmt.Println("Checking HTML:", html)
+	validHtml := false
+	s := stack.NewStack()
+	for idx, token := range  strings.Split(html, " ") {
+		if strings.Contains(token, "</") {
+			openingTab, err := s.Pop()
+			if err != nil || strings.Replace(token, "/", "", 1) != openingTab {
+				fmt.Println("errore in pos", idx + 1)
+				validHtml = false
+				break
+			}
+		} else {
+			s.Push(token)
+		}
+	}
+
+	if validHtml {
+		if s.Len() > 0 {
+			fmt.Println("still present opening tag ")
+			result := "Sono rimasti aperti i seguenti tag: "
+			for s.Len() > 0 {
+				operand, _ := s.Pop()
+				result += operand.(string) + " "
+			}
+			fmt.Println(result)
+		} else {
+			fmt.Println("HTML corretto")
+		}
+	}
+
+}
 
 func converti(seq string) string {
 	s := stack.NewStack()
