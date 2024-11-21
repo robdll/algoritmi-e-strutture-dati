f2ee1d313e047f57244655570abb045b13c4cafa
Roberto Di Lillo <roberto.dilillo@garycom.com>
2024-11-21 09:58:12 +0100

    adds linked list

---
+++ Lesson_15/01/go.mod
@@ -0,0 +1,3 @@
+module linkedList_project
+
+go 1.23.2
---
+++ Lesson_15/01/linkedList/linkedList.go
@@ -0,0 +1,86 @@
+package linkedlist
+
+import "fmt"
+
+type ListNode struct {
+	value int
+	next *ListNode
+}
+
+type LinkedList struct {
+	head *ListNode
+}
+
+//  creates and returns a new LinkedList.
+func NewLinkedList() *LinkedList {
+	return &LinkedList{head: nil}
+}
+
+func newNode (value int) *ListNode {
+	return &ListNode{value: value}
+}
+
+// complessità O(1)
+func AddNode (list *LinkedList, value int) {
+	node := newNode(value)
+	node.next = list.head
+	list.head = node
+	fmt.Println("Valore inserito")
+}
+
+// complessità O(n)
+func printList (list *LinkedList, ascend bool) {
+	var elements []int
+	for node := list.head; node != nil; node = node.next {
+		elements = append(elements, node.value)
+	}
+	if ascend {
+		for i := len(elements) - 1; i >= 0; i-- {
+			fmt.Println(elements[i])
+		}
+	} else {
+		for i := 0; i < len(elements); i++ {
+			fmt.Println(elements[i])
+		}
+	}
+}
+
+// complessità O(n)
+func searchList (list *LinkedList, value int) bool {
+	found := false
+	for node := list.head; node != nil; node = node.next {
+		if node.value == value {
+			found = true
+		}
+	}
+	return found
+}
+
+// complessità O(n)
+func removeNode (list *LinkedList, value int) {
+	if list.head == nil {
+		fmt.Println("Nessun elemento nella lista")
+		return
+	}
+	if list.head.value == value {
+		list.head = list.head.next
+		fmt.Println("Valore rimosso")
+		return
+	}
+	for node := list.head; node.next != nil; node = node.next {
+		if node.next.value == value {
+			node.next = node.next.next
+			fmt.Println("Valore rimosso")
+			return
+		}
+	}
+}
+
+// complessità O(n)
+func countElements (list *LinkedList) {
+	count := 0
+	for node := list.head; node != nil; node = node.next {
+		count++
+	}
+	fmt.Println("Total elements in the list:", count)
+}
