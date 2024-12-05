e1ff03ae3352193fcd07e39774297f1450fd0623
Roberto Di Lillo <roberto.dilillo@garycom.com>
2024-12-05 09:37:28 +0100

    Co-authored-by: AdrianoMaddalena <AdrianoMaddalena@users.noreply.github.com>

--- Lesson_16/01/Sunnydale.go
+++ Lesson_16/01/Sunnydale.go
@@ -8,27 +8,39 @@ import (
 	"strings"
 )
 
-type listNode struct {
-	destinazione int
-	peso int
-	next *listNode
+type Junction struct {
+	id int
+	galleries []*Gallery
+	visited bool
 }
 
-type linkedList struct {
-	iniziolista *listNode
+type Gallery struct {
+	from int
+	to int
+	brightness int
 }
 
+// func AddEdge(graph []*linkedList, from int, to int , peso int){
+// 	newNode := &listNode{destinazione: to, peso: peso, next: graph[from].iniziolista}
+// 	graph[from].iniziolista = newNode
+// }
 
 
-type informazioni struct{
-	svincoli, gallerie, posHarm, posSarah int
-}
-
+// func PrintGraph( graph []*linkedList){
+// 	for i,lista:=range graph{
+// 		if lista.iniziolista != nil {
+// 			Print(" nodo", i , ":")
+// 			current := lista.iniziolista
+// 			for current != nil{
+// 				Print(" -> ",current.destinazione, "( peso: ", current.peso, ")" )
+// 				current = current.next
+// 			}
+// 			Println()
+// 		}
+// 	}
+// }
 
 func main(){
-
-
-	var info informazioni
 
 	file, err := os.Open("VampireDates.txt")
 	if err != nil {
@@ -36,31 +48,59 @@ func main(){
 		return
 	}
 	defer file.Close()
-
-	// Scanner per leggere il file riga per riga
 	scanner := bufio.NewScanner(file)
 
+	var startPath, endPath int
+
 	if scanner.Scan(){
-		Input := strings.Fields(scanner.Text())
-
-		info.svincoli,_ = strconv.Atoi(Input[0])
-		info.gallerie,_ = strconv.Atoi(Input[1])
-		info.posHarm,_ = strconv.Atoi(Input[2])
-		info.posSarah,_ = strconv.Atoi(Input[3])
-
-		// Inizializza il grafo
-		graph := make([]*linkedList, info.svincoli+1) // Usa 1-based indexing
-		for i := range graph {
-			graph[i] = &linkedList{}
-		}
-		Print()
+		input := strings.Fields(scanner.Text())
+		start, _ := strconv.Atoi(input[2])
+		end, _ := strconv.Atoi(input[3])
+		startPath = start
+		endPath = end
 	}
+
+
+	junctionMap := make(map[int]Junction)
+		
+	for scanner.Scan() {
+		input := strings.Fields(scanner.Text())
+		from, _ := strconv.Atoi(input[0])
+		to, _ := strconv.Atoi(input[1])
+		brightness, _ := strconv.Atoi(input[2])
+
+		if junction, exists := junctionMap[from]; exists{
+			var newGallery = &Gallery{from, to, brightness}
+			junction.galleries = append(junctionMap[from].galleries, newGallery)
+			junctionMap[from] = junction
+		}	else {
+			var gallery = []*Gallery{{from, to, brightness}}
+			var junction = Junction{id: from, galleries: gallery, visited: false}
+			junctionMap[from] = junction
+		}
+	}
+
+	currentJunction, _ := junctionMap[startPath]
+	endJunction, _ := junctionMap[endPath]
 	
+	var steps int
+	for currentJunction.id != endJunction.id && currentJunction.visited == false {
+		steps++
+		currentJunction.visited = true
+		var darkestGallery *Gallery
+		for _, gallery := range currentJunction.galleries {
+			if darkestGallery == nil || gallery.brightness < darkestGallery.brightness {
+				darkestGallery = gallery
+			}
+		}
+		Println("going to: ", darkestGallery.to)
+		currentJunction = junctionMap[darkestGallery.to]
+	}
 
 
-	
-
-	 
-	
-
+	if currentJunction.id == endJunction.id {
+		Println("arrived in ", steps, "steps")
+	} else {
+		Println("Loop after #", steps, "steps")
+	}
 }
