32757e6c989086801ad0536481a1ed479075d506
AdrianoMaddalena <adrianomaddalena@MacBook-Air-di-Adriano.local>
2024-12-11 13:18:09 +0100

    exercise snake update

--- Lesson_16/02/snake.go
+++ Lesson_16/02/snake.go
@@ -22,7 +22,7 @@ func main(){
 	}
 	defer file.Close()
 	var board [30]int
-	var i int = 0
+	//var i int = 0
 	for i := 1; i<30; i++ {
 		board[i] = 0
 	}
@@ -39,8 +39,48 @@ func main(){
 	// Println("numero random 1: ", throwDice())
 	Println("board", board)
 
-	playGame(i, board)
+	//playGame(i, board)
+	mosseMinime := MosseMinimeSenzaUsoScaleSerpenti(board)
+	Println(mosseMinime)
 
+}
+
+func MosseMinimeSenzaUsoScaleSerpenti(board [30]int) int{
+		
+	lancioMax:= 6
+	var casellacorrente,verify int	//si parte dalla prima casella
+	var lanci int
+	//provo simulando il lancio con numero piu alto, se ciò porta ad una scala o serprente
+	//o superi la casella 30 il lancio sara rifatto dalla casella diminuito di uno il dado
+	for {
+		
+		if casellacorrente < 29{
+			
+			verify = board[casellacorrente]
+			Print("Prendo ",verify)
+			if verify != 0 {
+				
+				casellacorrente -= lancioMax
+				lancioMax -= 1
+				casellacorrente += lancioMax
+				lanci --
+				Println("if",casellacorrente)
+			}else{
+				lancioMax = 6
+				casellacorrente += lancioMax
+				lanci ++
+				Println(" lancio 6 procedi casella: ",casellacorrente)
+			}
+		}else if casellacorrente > 29{
+			Print("sei andato in over")
+			break
+		}else if casellacorrente == 29 {
+			Print("hai vinto con: ")
+			break
+		}
+	}
+
+	return lanci
 }
 
 
