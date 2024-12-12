b12a1d5144dff0dbf82db4f59ef75bbaace7da63
AdrianoMaddalena <adrianomaddalena10@gmail.com>
2024-12-12 08:18:03 +0100

    snake script
    * exercise snake update
    
    * nuova funzione
    
    * miglioramenti
    
    ---------
    
    Co-authored-by: AdrianoMaddalena <adrianomaddalena@MacBook-Air-di-Adriano.local>

--- Lesson_16/02/snake.go
+++ Lesson_16/02/snake.go
@@ -22,7 +22,7 @@ func main(){
 	}
 	defer file.Close()
 	var board [30]int
-	var i int = 0
+	var i int = 0 //casella i + 1
 	for i := 1; i<30; i++ {
 		board[i] = 0
 	}
@@ -38,9 +38,64 @@ func main(){
 
 	// Println("numero random 1: ", throwDice())
 	Println("board", board)
+	Println("inserisci la casella di partenza: ")
+	for{
+		Scan(&i)
+		if board[i] != 0{
+			Println("posizione non valida, inserisci un'altra casella: ")
+		}else{
+			break
+		}
 
-	playGame(i, board)
+	}
+	//playGame(i, board)
+	mosseMinime := MosseMinimeSenzaUsoScaleSerpenti(board, i+1)
+	Println(mosseMinime)
 
+
+	// casella di paretenza i + 1 da fare dopo
+}
+
+func MosseMinimeSenzaUsoScaleSerpenti(board [30]int, startbox int) int{
+		
+	maxThrow:= 6
+	var verify int = 0	//si parte dalla prima casella data i
+	var throwTOT int
+	//var dadi []int 	se vogliamo dire la serie dei dadi lanciati...
+	//provo simulando il lancio con numero piu alto, se ciò porta ad una scala o serprente
+	//o superi la casella 30 il lancio sara rifatto dalla casella diminuito di uno il dado
+	for {
+		
+		if startbox < 29{
+			
+			verify = board[startbox]
+			Print("Prendo ",verify)
+			if verify != 0 {
+				
+				startbox -= maxThrow
+				maxThrow -= 1
+				startbox += maxThrow
+				
+				Println("if",startbox)
+			}else{
+				maxThrow = 6
+				startbox += maxThrow
+				throwTOT ++
+				Println(" lancio 6 procedi casella: ",startbox)
+			}
+		}else if startbox > 29{
+			
+			startbox -=maxThrow
+			lancioperf := 29 - startbox
+			Print("sei andato in over, dovevi fare: ", lancioperf)
+			break
+		}else if startbox == 29 {
+			Print("hai vinto con: ")
+			break
+		}
+	}
+	//Println(dadi)
+	return throwTOT
 }
 
 
